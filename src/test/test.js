c417587ea484768353dc0e7e82478886ff695158
Ho0dLuM <Jordon.Hoshor@gmail.com>
2016-08-07 20:24:55 -0600

    added banner with text and calendar

---
+++ src/test/test.js
@@ -0,0 +1,65 @@
+// var chai = require('chai');
+// var expect = chai.expect;
+// //var should = chai.should();
+// var jsdom = require('mocha-jsdom');
+// $ = require('jsdom-global')();
+// //var main = require('../js/main.js');
+//
+//
+// var someModule = require('../js/main.js');
+//
+// describe('someModule', function() {
+//
+//   // create some jsdom magic to allow jQuery to work
+//   var doc = jsdom.jsdom(html),
+//       window = doc.parentWindow,
+//       $ = global.jQuery = require('jquery')(window);
+//
+//   it('does stuff', function() {
+//     someModule.doStuff();
+//     $('div.some-module').length.should.equal(1);
+//   });
+// });
+//main.js test function
+// function someModule() {
+// }
+// module.exports = someModule;
+//
+// someModule.doStuff = function() {
+//   jQuery('body').append('<div class="some-module"></div>');
+// }
+
+
+
+
+// var expect = chai.expect;
+// var should = chai.should();
+//     assert = require('assert'),
+//     jsdom = require('jsdom');
+
+// var jsdom = require('mocha-jsdom')
+// $ = require('jsdom-global')();
+// var main = require('../js/main.js');
+// var expect = chai.expect;
+// var should = chai.should();
+//
+// describe('', function() {
+//   it('1 should equal 1', function() {
+//     expect(1).to.equal(1);
+//   });
+// });
+//
+//
+//
+//   var $
+//   jsdom()
+//
+//   before(function () {
+//     $ = require('jquery')
+//   });
+//   describe('shit worked', function () {
+//     it('should create <div> indicating shit worked', function () {
+//       expect($(h1).append()).to.equal('sanity check!');
+//   });
+//
+// });
