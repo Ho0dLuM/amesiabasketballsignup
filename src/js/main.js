4ef7bc89fba7359ad402d04008810da0f4eb2d24
Ho0dLuM <Jordon.Hoshor@gmail.com>
2016-08-10 15:32:15 -0600

    test

--- src/js/main.js
+++ src/js/main.js
@@ -1,108 +1,126 @@
-(function() {
-  'use strict';
+$(document).ready(function() {
+  $('#calendar').fullCalendar({
+  });
+});
+/*
+neat comments!
+there may be some places where another function could be created for the conditionals, but I'm not sure if that would break your code or not.
+good stuff!
+*/
+//Add e-mail to cell
+
+// var emailObj = ajaxCall(apiUrl);
+// var apiUrl = 'https://us1.api.mailchimp.com/3.0/automations/1cd819280d/emails/2117f379e7/queue'
 
 
-  // Your Client ID can be retrieved from your project in the Google
-  // Developer Console, https://console.developers.google.com
-  var CLIENT_ID = '104605432494974692930';
-
-  var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
-
-  /**
-   * Check if current user has authorized this application.
-   */
-  function checkAuth() {
-    gapi.auth.authorize(
-      {
-        'client_id': CLIENT_ID,
-        'scope': SCOPES.join(' '),
-        'immediate': true
-      }, handleAuthResult);
-  }
-
-  /**
-   * Handle response from authorization server.
-   *
-   * @param {Object} authResult Authorization result.
-   */
-  function handleAuthResult(authResult) {
-    var authorizeDiv = document.getElementById('authorize-div');
-    if (authResult && !authResult.error) {
-      // Hide auth UI, then load client library.
-      authorizeDiv.style.display = 'none';
-      loadCalendarApi();
-    } else {
-      // Show auth UI, allowing the user to initiate authorization by
-      // clicking authorize button.
-      authorizeDiv.style.display = 'inline';
-    }
-  }
-
-  /**
-   * Initiate auth flow in response to user clicking authorize button.
-   *
-   * @param {Event} event Button click event.
-   */
-  function handleAuthClick(event) {
-    gapi.auth.authorize(
-      {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-      handleAuthResult);
-    return false;
-  }
-
-  /**
-   * Load Google Calendar client library. List upcoming events
-   * once client library is loaded.
-   */
-  function loadCalendarApi() {
-    gapi.client.load('calendar', 'v3', listUpcomingEvents);
-  }
-
-  /**
-   * Print the summary and start datetime/date of the next ten events in
-   * the authorized user's calendar. If no events are found an
-   * appropriate message is printed.
-   */
-  function listUpcomingEvents() {
-    var request = gapi.client.calendar.events.list({
-      'calendarId': 'primary',
-      'timeMin': (new Date()).toISOString(),
-      'showDeleted': false,
-      'singleEvents': true,
-      'maxResults': 10,
-      'orderBy': 'startTime'
-    });
-
-    request.execute(function(resp) {
-      var events = resp.items;
-      appendPre('Upcoming events:');
-
-      if (events.length > 0) {
-        for (i = 0; i < events.length; i++) {
-          var event = events[i];
-          var when = event.start.dateTime;
-          if (!when) {
-            when = event.start.date;
-          }
-          appendPre(event.summary + ' (' + when + ')')
-        }
-      } else {
-        appendPre('No upcoming events found.');
+  $('#calendar').fullCalendar({
+    dayClick: function(date, cell) {
+      var userEmail = window.prompt('Enter your E-mail adress to sign up to play on ' + date.format());
+        //validate input from prompt
+        var atPos = userEmail.indexOf('@');
+        var dotPos = userEmail.lastIndexOf('.');
+        if(userEmail === 0 || atPos < 1 || dotPos < atPos + 2 || dotPos + 2 >= userEmail.length) {
+          return;
+        } else {
+          //when validation passes
+        $(cell.target).append(userEmail + '<br>');
+        $(this).css({'background-color':'#CC0000'})
+        cellCheck();
       }
+    }
+  });
 
+//watcher function start
+  function cellCheck() {
+    $('td.fc-day').filter(function (index, element) {
+    var cellFind = $(element).text() === 'jordon.hoshor@gmail.com';
+    //console.log(cellFind);
+      if(cellFind === true) {
+      $.ajax({
+        url: 'https://us1.api.mailchimp.com/3.0/automations/1cd819280d/emails/2117f379e7/queue',
+  				type: 'POST',
+          dataType: 'JSON',
+  				data: {
+                apikey: '9e6c86ce5121df997998a5feb8d25ee9-us1',
+  				      email: 'jordon.hoshor@gmail.com',
+                status: 'subscribed'
+  				      },
+  				success: function(data){
+  					alert('success')
+  				},
+  				error: function(err) {
+  					console.log('fail', err);
+  				}
+        });
+      };
     });
   }
+//mailchimp APi key = 9e6c86ce5121df997998a5feb8d25ee9-us1
 
-  /**
-   * Append a pre element to the body containing the given message
-   * as its text node.
-   *
-   * @param {string} message Text to be placed in pre element.
-   */
-  function appendPre(message) {
-    var pre = document.getElementById('output');
-    var textContent = document.createTextNode(message + '\n');
-    pre.appendChild(textContent);
-  }
 
-}());
+
+//$.post('https://us1.api.mailchimp.com/3.0/automations/1cd819280d/emails/2117f379e7/queue', function() {
+
+//   $.post( "example.php", function() {
+//     alert( "success" );
+//   })
+//
+//
+// function ajaxCall(url) {
+//   return Promise.resolve($.ajax(apiUrl));
+// }
+//
+
+  // document.addEventListener("contextmenu", function(e) {
+  //   e.preventDefault();
+  // }, false);
+  // document.addEventListener("contextmenu", function(e) {
+  //   var removeEmail = window.prompt('Enter your Email for removal');
+  //   var atPos = removeEmail.indexOf('@');
+  //   var dotPos = removeEmail.lastIndexOf('.');
+  //     if (e.which === 3) {
+  //   } else if(removeEmail === 0 || atPos < 1 || dotPos < atPos + 2 || dotPos + 2 >= removeEmail.length) {
+  //     return;
+  //   } else {
+  //     removeEmail.replace(0, '');
+  //   }
+  // });
+
+
+  // //Remove email from cell listener
+  //   $('form').on('submit', function(event) {
+  //     event.preventDefault();
+  //     var emailToRemove = $('#removeEmail').val();
+  //     //console.log(emailToRemove);
+  //     $('td.fc-day').filter(function (index, element) {
+  //       $(element).text() === emailToRemove;
+  //         $('emailToRemove').remove();
+  //     });
+  //   });
+
+
+
+ //you can inspect the click event
+//
+//     $this = $('.cmenu');
+//
+//     if (e.which === 3){ // right click = 3, left click = 1
+//         $this.addClass('open');
+//         $this.css({
+//             'left': e.pageX - $this.width() / 2,
+//             'top': e.pageY - $this.height()
+//         });
+//     }else{
+//         $this.removeClass('open');
+//     }
+// });
+
+
+
+
+  // $('td.fc-day').filter(function (index, element) {
+  //   return $(element).text() === 'email@email.com'
+  // })
+  // $('td.fc-day').filter(function (index, element) {
+  //   console.log(element.innerHTML);
+  // });
